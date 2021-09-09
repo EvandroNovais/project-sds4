db25366d6d227c32df7912b50dad28faf7a75db3
Evandro Novais <evandro-novais@outlook.com>
2021-09-08 22:10:04 -0300

    Created projects

---
+++ backend/src/test/java/com/evandro/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evandro.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
