1db375132a65f9f1f56ff4471ba2f90956da373d
Evandro Novais <evandro-novais@outlook.com>
2021-09-10 00:36:11 -0300

    Group by search

--- backend/src/main/java/com/evandro/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/evandro/dsvendas/controllers/SaleController.java
@@ -1,5 +1,7 @@
 package com.evandro.dsvendas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.evandro.dsvendas.dto.SaleDTO;
+import com.evandro.dsvendas.dto.SaleSuccessDTO;
+import com.evandro.dsvendas.dto.SaleSumDTO;
 import com.evandro.dsvendas.services.SaleService;
 
 @RestController
@@ -21,6 +25,18 @@ public class SaleController {
 	@GetMapping
 	public ResponseEntity<Page<SaleDTO>> findAll(Pageable page){
 		Page<SaleDTO> list = saleService.findAll(page);
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller(){
+		List<SaleSumDTO> list = saleService.amountGroupedBySeller();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/success-by-seller")
+	public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller(){
+		List<SaleSuccessDTO> list = saleService.successGroupedBySeller();
 		return ResponseEntity.ok(list);
 	}
 }
--- backend/src/main/java/com/evandro/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/evandro/dsvendas/services/SaleService.java
@@ -1,5 +1,7 @@
 package com.evandro.dsvendas.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.evandro.dsvendas.dto.SaleDTO;
+import com.evandro.dsvendas.dto.SaleSuccessDTO;
+import com.evandro.dsvendas.dto.SaleSumDTO;
 import com.evandro.dsvendas.entities.Sale;
 import com.evandro.dsvendas.repositories.SaleRepository;
 import com.evandro.dsvendas.repositories.SellerRepository;
@@ -25,5 +29,13 @@ public class SaleService {
 		sellerRepository.findAll();
 		Page<Sale> result = saleRepository.findAll(page);
 		return result.map(x -> new SaleDTO(x));
+	}
+	
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		return saleRepository.amountGroupedBySeller();
+	}
+	
+	public List<SaleSuccessDTO> successGroupedBySeller(){
+		return saleRepository.successGroupedBySeller();
 	}
 }
